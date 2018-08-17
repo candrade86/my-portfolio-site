4fb832fe660633da2300a5621c79879fa490c416
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-17 14:26:33 -0400

    debugging nav

--- src/components/App.js
+++ src/components/App.js
@@ -20,53 +20,53 @@ import {
 
 
 class App extends Component {
-  componentDidMount(){
-    window.onscroll = function() {myFunction()};
-    let nav = document.getElementById('navbar');
-    let headerLink = document.getElementById('header-link'); 
-    let aboutLink = document.getElementById('about-link');
-    let portfolioLink = document.getElementById('portfolio-link');
-    let blogLink = document.getElementById('blog-link');
-    let contactLink = document.getElementById('contact-link');
-    let sticky = nav.offsetTop;
+  // componentDidMount(){
+  //   window.onscroll = function() {myFunction()};
+  //   let nav = document.getElementById('navbar');
+  //   let headerLink = document.getElementById('header-link'); 
+  //   let aboutLink = document.getElementById('about-link');
+  //   let portfolioLink = document.getElementById('portfolio-link');
+  //   let blogLink = document.getElementById('blog-link');
+  //   let contactLink = document.getElementById('contact-link');
+  //   let sticky = nav.offsetTop;
 
-    function myFunction() {
-      if (window.pageYOffset > sticky) {
-        nav.classList.add('sticky');
-        headerLink.classList.add('link-style');
-        aboutLink.classList.add('link-style');
-        portfolioLink.classList.add('link-style');
-        blogLink.classList.add('link-style');
-        contactLink.classList.add('link-style');
-      } else {
-        nav.classList.remove('sticky');
-        headerLink.classList.remove('link-style');
-        aboutLink.classList.remove('link-style');
-        portfolioLink.classList.remove('link-style');
-        blogLink.classList.remove('link-style');
-        contactLink.classList.remove('link-style');
-      }
-    }
-  }
+  //   function myFunction() {
+  //     if (window.pageYOffset > sticky) {
+  //       nav.classList.add('sticky');
+  //       headerLink.classList.add('link-style');
+  //       aboutLink.classList.add('link-style');
+  //       portfolioLink.classList.add('link-style');
+  //       blogLink.classList.add('link-style');
+  //       contactLink.classList.add('link-style');
+  //     } else {
+  //       nav.classList.remove('sticky');
+  //       headerLink.classList.remove('link-style');
+  //       aboutLink.classList.remove('link-style');
+  //       portfolioLink.classList.remove('link-style');
+  //       blogLink.classList.remove('link-style');
+  //       contactLink.classList.remove('link-style');
+  //     }
+  //   }
+  // }
   render() {
 
     return (
       <AppWrapper>
-          <Nav id="navbar">
+          {/* <Nav id="navbar">
             <LinkWrap><AnchorLink id='header-link' href='#header'>Header</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink id='about-link' href='#about'>About</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink id='portfolio-link' href='#portfolio'>Portfolio</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink id='blog-link' href='#blog'>blog</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink id='contact-link' href='#contact'>Contact</AnchorLink></LinkWrap>
-          </Nav> 
+          </Nav>  */}
 
-           {/* <Nav>
+           <Nav>
             <LinkWrap><AnchorLink href='#header'>Header</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink href='#about'>About</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink href='#portfolio'>Portfolio</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink href='#blog'>blog</AnchorLink></LinkWrap>
             <LinkWrap><AnchorLink href='#contact'>Contact</AnchorLink></LinkWrap>
-          </Nav>  */}
+          </Nav> 
             
         <Header id={'header'} />
         <About id={'about'} />
