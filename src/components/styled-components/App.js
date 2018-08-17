f54c136d1a5639916ef559758a60cb8c072bee9d
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-17 13:56:09 -0400

    commented out positioning borders on component wrappers

--- src/components/styled-components/App.js
+++ src/components/styled-components/App.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const AppWrapper = styled.div`
     margin: 0;
     height: 100vh;
-    border: solid purple;
+
+    /* border: solid purple; */
 
 `
 
