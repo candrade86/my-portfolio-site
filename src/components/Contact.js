bdb8744b0042559efa7ae6eafd5f2f7e14ee694d
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-26 20:49:27 -0400

    completed styling and functionality for the  contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { backendAPI } from '../config.js'
 
+import { backendAPI } from '../config.js'
 import { 
-    Section 
-} from './styled-components/About';
+    Section,
+    FormWrap 
+} from './styled-components/Contact';
+
+import './animation-css/Contact.css';
+import $ from 'jquery';
+
+import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
+
+import axios from 'axios';
+
 
 class Contact extends Component {
 
@@ -14,19 +23,20 @@ class Contact extends Component {
         const email = document.getElementById('email').value;
         const message = document.getElementById('message').value;
         axios({
-            method: "POST", 
-            url: 'http://localhost:5000/api/send', 
+            method: 'POST', 
+            url: 'https://peaceful-taiga-12647.herokuapp.com/api/send', 
             data: {
                 name: name,   
                 email: email,  
                 message: message
             }
         }).then((response)=>{
+            console.log(response)
             if (response.data.msg === 'success'){
-                alert("Message Sent."); 
+                alert('Message Sent.'); 
                 this.resetForm()
             }else if(response.data.msg === 'fail'){
-                alert("Message failed to send.")
+                alert('Message failed to send.')
             }
         })
     }
@@ -38,25 +48,49 @@ class Contact extends Component {
     render() {
         return (
             <Section id={this.props.id}>
-                {console.log("conact props", this.props.id)}
-                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                
+                {/* <form id='contact-form' onSubmit={this.handleSubmit.bind(this)} method='POST'>
                     <div>
-                        <label for="name">Name</label>
-                        <input type="text" id="name" />
+                        <label for='name'>Name</label>
+                        <input type='text' id='name' />
                     </div>
-                    <div className="form-group">
-                        <label for="InputEmail1">Email address</label>
-                        <input type="email" id="email" aria-describedby="emailHelp" />
+                    <div className='form-group'>
+                        <label for='InputEmail1'>Email address</label>
+                        <input type='email' id='email' aria-describedby='emailHelp' />
                     </div>
                     <div>
-                        <label for="message">Message</label>
-                        <textarea id="message"></textarea>
+                        <label for='message'>Message</label>
+                        <textarea id='message'></textarea>
                     </div>
-                    <button type="submit">Submit</button>
-                </form>         
+                    <button type='submit'>Submit</button>
+                </form>          */}
+                 <ScrollAnimation animateIn='fadeInUp' style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <FormWrap>
+                        <form id='contact-form' className='contact' onSubmit={this.handleSubmit.bind(this)} method='POST'>
+                            <h3>Contact me!</h3>
+            
+                            <fieldset>
+                            <input id='name' placeholder='Your name' type='text' tabindex='1' required autofocus />
+                            </fieldset>
+                            <fieldset>
+                            <input id='email' placeholder='Your Email Address' type='email' tabindex='2' required />
+                            </fieldset>
+                            
+                            <fieldset>
+                            <textarea id='message' placeholder='Type your message here....' tabindex='3' required></textarea>
+                            </fieldset>
+
+                            
+                            <fieldset>
+                            <button name='submit' type='submit' id='contact-submit' >Submit</button>
+                            </fieldset>
+                        </form>
+                    </FormWrap>
+                </ScrollAnimation>
             </Section>
         );
     }
 }
 
 export default Contact;
+
--- src/components/styled-components/Contact.js
+++ src/components/styled-components/Contact.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
 
 export const Section = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     position: relative;
     z-index: 2;
     height: 100vh;
 
-    background: yellow;
+    background: lightgrey;
     /* border: solid green; */
+`
+
+export const FormWrap = styled.div`
+    width: 50%;
 `
