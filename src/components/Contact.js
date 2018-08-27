93391712f8d163dcb6d16fe2f59f5d5d87a257aa
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-26 22:12:48 -0400

    fixed the styling issue I was having when deploying the app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,7 +24,7 @@ class Contact extends Component {
         const message = document.getElementById('message').value;
         axios({
             method: 'POST', 
-            url: 'https://peaceful-taiga-12647.herokuapp.com/api/send', 
+            url: backendAPI, 
             data: {
                 name: name,   
                 email: email,  
@@ -48,22 +48,6 @@ class Contact extends Component {
     render() {
         return (
             <Section id={this.props.id}>
-                
-                {/* <form id='contact-form' onSubmit={this.handleSubmit.bind(this)} method='POST'>
-                    <div>
-                        <label for='name'>Name</label>
-                        <input type='text' id='name' />
-                    </div>
-                    <div className='form-group'>
-                        <label for='InputEmail1'>Email address</label>
-                        <input type='email' id='email' aria-describedby='emailHelp' />
-                    </div>
-                    <div>
-                        <label for='message'>Message</label>
-                        <textarea id='message'></textarea>
-                    </div>
-                    <button type='submit'>Submit</button>
-                </form>          */}
                  <ScrollAnimation animateIn='fadeIn' style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                     <FormWrap>
                         <form id='contact-form' className='contact' onSubmit={this.handleSubmit.bind(this)} method='POST'>
