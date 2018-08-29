166fa35c471556a729a2fec15975925c24e640bc
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-29 01:31:43 -0400

    error on deployment with responsiveness

--- src/components/styled-components/Header.js
+++ src/components/styled-components/Header.js
@@ -43,6 +43,34 @@ export const Button = styled.button`
     color: white;
     border: 5px solid white;
     background-color: transparent;
+
+    @media (max-width: 981px) {
+        width: 30%;
+    }
+
+    @media (max-width: 604px) {
+        width: 35%;
+    }
+
+     @media (max-width: 507px) {
+        width: 40%;
+    }
+
+    @media (max-width: 436px) {
+        width: 45%;
+    }
+
+    @media (max-width: 383px) {
+        width: 50%;
+    }
+
+    @media (max-width: 342px) {
+        width: 55%;
+    }
+
+    @media (max-width: 308px) {
+        width: 60%;
+    }
 `
 export const TitleWrap = styled.div`
     color: white;
@@ -51,5 +79,66 @@ export const TitleWrap = styled.div`
 `
 export const Title = styled.h1`
     font-size: 10rem;
+
+      @media (max-width: 707px) {
+        font-size: 9rem;
+  }
+
+       @media (max-width: 636px) {
+        font-size: 8.5rem;
+  }
+
+        @media (max-width: 601px) {
+        font-size: 8rem;
+  }
+
+
+     @media (max-width: 567px) {
+        font-size: 7.5rem;
+  }
+
+     @media (max-width: 530px) {
+        font-size: 7rem;
+  }
+
+       @media (max-width: 495px) {
+        font-size: 6.5rem;
+  }
+
+     @media (max-width: 473px) {
+        font-size: 6rem;
+  }
+
+     @media (max-width: 435px) {
+        font-size: 5.5rem;
+  }
+
+   @media (max-width: 417px) {
+        font-size: 5rem;
+  }
+
+    @media (max-width: 397px) {
+        font-size: 4.5rem;
+  }
+
+   @media (max-width: 378px) {
+        font-size: 4rem;
+  }
+
+   @media (max-width: 360px) {
+        font-size: 3.5rem;
+  }
+
+  @media (max-width: 342px) {
+        font-size: 3rem;
+  }
+
+  @media (max-width: 322px) {
+        font-size: 2.5rem;
+  }
+
+  @media (max-width: 303px) {
+        font-size: 2rem;
+  }
 `
 
